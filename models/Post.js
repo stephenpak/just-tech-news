d36cbfaf0b7a56868fa2fccd396e61acac52bf4c
Stephen Pak <stephenjpak@gmail.com>
2022-05-27 01:29:42 -0700

    5-27-22-1:29am

--- models/Post.js
+++ models/Post.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 // create our Post model
 class Post extends Model {}
+
 // create fields/columns for Post model
 Post.init(
   {
