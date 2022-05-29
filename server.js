c261b91dcd57ebd148882659a209a500d92f48db
Stephen Pak <stephenjpak@gmail.com>
2022-05-28 23:59:43 -0700

    5-28-22-11:59

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // turn on routes
 app.use(routes);
