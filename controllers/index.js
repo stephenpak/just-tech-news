db366c4407213cdda3b219a7792b2325ffebc4d0
Stephen Pak <stephenjpak@gmail.com>
2022-06-02 01:30:48 -0700

    6-2-22-1:30am

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,18 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
+});
+
+router.get('/', (req, res) => {
+  res.render('homepage');
 });
 
 module.exports = router;
