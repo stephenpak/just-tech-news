dd2e79a41b14883fdfc7a375975837f375d6b007
Stephen Pak <stephenjpak@gmail.com>
2022-06-05 19:10:35 -0700

    6-5-22-7:10pm

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {}, 
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
